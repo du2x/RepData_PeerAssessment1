b62f824f9b24fb9e8f81e124b6fa83cd3cef21f5
09713051700 <09713051700>
2016-02-05 15:02:41 -0200

    Added figure/ folder.

--- solution.R
+++ solution.R
@@ -12,23 +12,23 @@ mean(steps_per_day$steps, na.rm=TRUE)
 median(steps_per_day$steps, na.rm=TRUE)
 
 by_interval = group_by(data, interval)
-steps_per_interval = summarize(by_interval, mean(steps, na.rm=TRUE))
-names(steps_per_interval)<-c('interval', 'steps')
-steps_per_interval[which.max(steps_per_interval$steps), ]
-mean_interval <- mean(steps_per_interval$steps)
+spi = summarize(by_interval, mean(steps, na.rm=TRUE))
+names(spi)<-c('interval', 'steps')
+spi[which.max(spi$steps), ]
+mean_interval <- mean(spi$steps)
 
 nas <- is.na(data$steps)
 num_nas <- sum(nas)
 data_no_nas <- data
 data_no_nas[nas, 'steps'] <- mean_interval
 by_interval_no_nas = group_by(data_no_nas, interval)
-steps_per_interval_no_nas = summarize(by_interval_no_nas, mean(steps, na.rm=TRUE))
-names(steps_per_interval_no_nas)<-c('interval', 'steps')
+spi_no_nas = summarize(by_interval_no_nas, mean(steps, na.rm=TRUE))
+names(spi_no_nas)<-c('interval', 'steps')
 
-steps_no_nas <- steps_per_interval_no_nas$steps
-steps <- steps_per_interval$steps
-stepsCombined <- rbind(data.frame(steps=steps, nas='Present'), data.frame(steps=steps_no_nas, nas='Filled with mean of intervals'))
-ggplot(stepsCombined, aes(steps, fill = nas)) + geom_density(alpha = 0.2)
+p1<-plot.ts(x=spi$interval, y=spi$steps, xlab= 'Interval', ylab = 'Steps')
+p2<-plot.ts(x=spi_no_nas$interval, y=spi_no_nas$steps, xlab= 'Interval', ylab = 'Steps')
+multiplot(p1, p2)
+@
 
 wkd <- weekdays(as.Date(data_no_nas$date))
 weekday = wkd!='Sábado' & wkd !='Domingo'
@@ -38,16 +38,16 @@ levels(weekday)[levels(weekday)==FALSE]='weekend'
 data_no_nas$daytype = weekday
 
 by_interval_no_nas_weekday = group_by(data_no_nas[data_no_nas$daytype=='weekday',], interval)
-steps_per_interval_no_nas_weekday = summarize(by_interval_no_nas_weekday,mean(steps, na.rm=TRUE))
-names(steps_per_interval_no_nas_weekday)<-c('interval', 'steps')
+spi_no_nas_weekday = summarize(by_interval_no_nas_weekday,mean(steps, na.rm=TRUE))
+names(spi_no_nas_weekday)<-c('interval', 'steps')
 
 by_interval_no_nas_weekend = group_by(data_no_nas[data_no_nas$daytype=='weekend',], interval)
-steps_per_interval_no_nas_weekend = summarize(by_interval_no_nas_weekend, mean(steps, na.rm=TRUE))
-names(steps_per_interval_no_nas_weekend)<-c('interval', 'steps')
+spi_no_nas_weekend = summarize(by_interval_no_nas_weekend, mean(steps, na.rm=TRUE))
+names(spi_no_nas_weekend)<-c('interval', 'steps')
 
 
 
-p1 <- qplot(x='interval', y='steps', data=steps_per_interval_no_nas_weekday)
-p2 <- qplot(x='interval', y='steps', data=steps_per_interval_no_nas_weekend)
+p1 <- qplot(x='interval', y='steps', data=spi_no_nas_weekday)
+p2 <- qplot(x='interval', y='steps', data=spi_no_nas_weekend)
 
 multiplot(p1, p2, cols=2)
